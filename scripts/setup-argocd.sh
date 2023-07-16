1a7e091f1287090142612a51f20744d06f1fa67b
adityasinghal26 <adiaggarwal26@gmail.com>
2023-07-16 11:53:13 +0000

    Added ArgoCD CLI installation

--- scripts/setup-argocd.sh
+++ scripts/setup-argocd.sh
@@ -9,3 +9,8 @@ helm install argocd \
 argo/argo-cd \
 --namespace argocd \
 --create-namespace
+
+# Install ArgoCD CLI and add it to PATH variables
+curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64 
+sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd 
+rm argocd-linux-amd64
